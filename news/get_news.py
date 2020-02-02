d10ab6b47f7244e480fb3cd5a8284da23d5c12b4
Amir Baer <amir@curv.cc>
2020-02-02 12:08:58 +0200

    new query mechanism works

--- news/get_news.py
+++ news/get_news.py
@@ -1,136 +1,111 @@
 #!/usr/local/bin/python3
 
-from GoogleNews import GoogleNews
+import bs4
+from bs4 import BeautifulSoup as soup
+import urllib.parse as urlparse
+from urllib.parse import urlencode
+from urllib.request import urlopen
+import sys
 
-coins = [
-            {
-                "tickers": ("btc", "bitcoin", "bitcoin foundation"),
-                "figures": ("satoshi nakamoto"),
-            },
-            {
-                "tickers": ("etc", "ethereum"),
-                "figures": ("vitalik buterin", "virgil griffith"),
-                "organizations": ("ethereum foundation",),
-            },
-            {
-                "tickers": ("eos", "eos.io"),
-                "figures": ("daniel larimer", "brendan blumer"),
-                "organizations": ("block.one", "eos foundation"),
-            },
-            {
-                "tickers": ("xrp",),
-                "figures": ("david schwartz",),
-                "organizations": ("ripple", "ripple labs"),
-            },
-            {
-                "tickers": ("ltc", "litecoin"),
-                "figures": ("charlie lee", "john kim"),
-                "organizations": ("litecoin core development team",),
-            },
-            {
-                "tickers": ("bch", "bitcoin cash", "bitcoin abc"),
-            },
-            {
-                "tickers": ("bnb", "binance"),
-                "figures": ("changpeng zhao",),
-                "organizations": ("binance charity foundation",),
-            },
-            {
-                "tickers": ("xlm", "stellar", "stellar lumens"),
-                "figures": ("jed mccaleb",),
-                "organizations": ("stellar development foundation",),
-            },
-            {
-                "tickers": ("bsv", "bitcoin  sv", "bitcoin satoshi vision"),
-                "figures": ("craig wright", "dr craig s wright", "calvin ayre"),
-                "organizations": ("nchain",),
-            },
-        ]
-
-verticals = [
-                "",
-                "attack", "theft",
-                "regulation", "regulatory",
-                "development", "roadmap",
-                "institutional adoption",
-                "announce", "announcement",
-            ]
-
-selection = [
-        "tickers",
-        "figures",
-        "organizations",
-        ]
-
+from entities import coins, verticals, selection
+from news_item import Item
 
 #### TESTING #####
 
-#coins = coins[:1]
-#verticals = verticals[:2]
-#selection = selection[:1]
+coins = coins[:1]
+verticals = verticals[:2]
+selection = selection[:1]
 
 #################
 
-"""
-def clean_results(orig):
-    res = 
-    for row in orig:
-"""
+def get_query_url(query):
+    news_url="https://news.google.com/news/rss"
+    params = {'q': query, 'lang':'en'}
+
+    url_parts = list(urlparse.urlparse(news_url))
+    query_dict = dict(urlparse.parse_qsl(url_parts[4]))
+    query_dict.update(params)
+
+    url_parts[4] = urlencode(query_dict)
+    return urlparse.urlunparse(url_parts)
+
+# TODO: paging
+def get_news_items(query, dewey=[]):
+    query_url = get_query_url(query)
+    client = urlopen(query_url)
+    xml_page = client.read()
+    client.close()
+
+    news_items = []
+    soup_page = soup(xml_page, "html.parser")
+    for news in soup_page.findAll("item"):
+        news_items.append(Item(
+            news.title.text,
+            news.link.text,
+            news.pubdate.text,
+            query,
+            dewey
+        ))
+
+    return news_items
+
+def print_news(news_items):
+    for item in news_items:
+      print(item.title)
+      print(item.link)
+      print(item.date)
+      print(item.query)
+      print("-"*60)
+
 
 def save_as_html(filename, ml):
     html = ""
-    for c in ml.keys():
-        html += "<h1>%s</h1>\n" % c
+    for (cn, v, cat), items in ml.items():
+        html += "<br><h2>%s :: %s :: %s</h2><br>\n" % (cn, v, cat)
 
-        for v in ml[c].keys():
-            html += "<h2>%s :: %s</h2>\n" % (c, v)
-
-            for cat in ml[c][v].keys():
-                html += "<h3>%s :: %s :: %s</h3>\n" % (c, v, cat)
-
-                for query, items in ml[c][v][cat].items():
-                    html += "<h4>%s</h4>" % query
-                    for item in items:
-                        html += '<a href="%s">%s</a>' % (item["link"], item["title"])
-                        html += "<br>"
-                        titles_seen.add(title)
-
-                html += "<br>"
-            html += "<br>"
-        html += "<br>"
+        if items:
+            html += "<br><h4>%s</h4><br>\n" % items[0].original_query
+            for item in items:
+                html += '<a href="%s">%s</a><br>\n' % (item.link, item.title)
 
     open(filename, 'w').write(html)
 
 
 def update():
-    gn = GoogleNews()
+    # dict: { (cn, v, cat): [item, item, ...] }
     res = {}
     titles_seen = set()
     for c in coins:
         cn = c["tickers"][0]
-        res.setdefault(cn, {})
-
         for v in verticals:
-            res[cn].setdefault(v, {})
-
             for cat, values in c.items():
                 if cat not in selection:
                     continue
-                res[cn][v].setdefault(cat, {})
 
                 for q in values:
                     fq = " ".join([q, v]) if v else q
-                    res[cn][v][cat].setdefault(fq, [])
+                    res.setdefault((cn, v, cat), [])
 
-                    # TODO: paging
-                    gn.search(fq)
-
-                    for item in gn.results:
-                        title = item["title"]
+                    for item in get_news_items(fq, [cn, v, cat]):
+                        title = item.title
                         if title in titles_seen:
                             print("skipping: %s" % title)
                             continue
 
-                        res[cn][v][cat][fq].append(item)
+                        res[(cn, v, cat)].append(item)
                         titles_seen.add(title)
     return res
+
+
+########## MAIN ###########
+
+def main(out_filename):
+    ml = update()
+    save_as_html(out_filename, ml)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: %s <output filename>" % sys.argv[0])
+        sys.exit(1)
+
+    main(sys.argv[1])
