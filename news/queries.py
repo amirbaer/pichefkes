6ce5df9a53029ecb6692a8d7e1a119ed9d1ea53c
Amir Baer <amir@curv.cc>
2020-03-09 14:20:10 +0200

    saving queries to git so that I can retain versions

--- news/queries.py
+++ news/queries.py
@@ -1,22 +1,124 @@
-#NOTE: add this file and parameter (list of strings)
-"""
-topic_queries = {
-    "topic1": (
-        "query1",
-        "query2",
-        "query3",
-    ),
-    "topic2": (
-        "query4",
-        "query5",
-        "query6",
-    ),
-]
-"""
-from my_queries import topic_queries
 
-"""
-# Option B
-from comb_query_generator import get_queries
-topic_queries = get_queries()
-"""
+topic_queries = {
+        ## Coins
+
+        "btc": (
+            "bitcoin",
+            "bitcoin top news",
+
+            "bitcoin development roadmap",
+            "bitcoin community discussion",
+            "bitcoin network attack",
+        
+            "bitcoin foundation",
+        ),
+
+        "eth": (
+            "ethereum",
+            "ethereum top news",
+
+            "ethereum development roadmap",
+            "ethereum community discussion",
+            "ethereum network attack",
+        
+            "'ethereum foundation'",
+
+            "vitalik buterin",
+        ),
+        
+            # EOS
+        "eos": (
+            "eos -canon -eos.org",
+            "eosio crypto",
+            "eos 'block.one'",
+            "'eos foundation' -eos.org",
+        ),
+
+            # XRP
+        "xrp": (
+            "xrp ripple",
+            "'ripple labs'",
+            "xpring",
+            "'ripple foundation'",
+        ),
+
+            # LTC
+        "ltc": (
+            "ltc litecoin",
+            "'litecoin core development team'",
+        ),
+
+            # BCH
+        "bch": (
+            "bch 'bitcoin cash'",
+        ),
+
+            # BNB
+        "bnb": (
+            "bnb binance",
+            "'binance charity foundation'",
+        ),
+
+            # XLM
+        "xlm": (
+            "xlm stellar",
+            "stellar lumens",
+            "'stellar development foundation'",
+        ),
+
+            # BSV
+        "bsv": (
+            "bsv 'bitcoin satoshi'",
+            "dr craig s wright",
+            "calvin ayre",
+            "nchain",
+        ),
+
+
+            ## Crypto Space
+
+        "crypto-space": (
+            "cryptocurrency custodians",
+            "cryptocurrency regulations",
+            "cryptocurrency network attack",
+            "cryptocurrency network theft sum",
+            "cryptocurrency emerging trend",
+            "cryptocurrency institutional adoption",
+            "cryptocurrency m&a",
+            "cryptocurrency major announcement",
+            "cryptocurrency new development roadmap",
+
+            "crypto custodians",
+            "crypto regulations",
+            "crypto network attack",
+            "crypto network theft sum",
+            "crypto emerging trend",
+            "crypto institutional adoption",
+            "crypto m&a",
+            "crypto major announcement",
+            "crypto new development roadmap",
+        ),
+
+
+            ## Competitors
+
+        "competitors": (
+            "bitgo",
+            "bitgo partners",
+            "bitgo raised",
+
+            #"unbound tech",
+            "unbound crypto",
+            "unbound crypto partners",
+            "unbound crypto raised",
+
+            '"fireblocks" -"fire blocks"',
+            '"fireblocks" partners',
+
+            "ledger wallet",
+            "ledger wallet partners",
+
+            "anchorage crypto",
+            "anchorage crypto partners",
+        ),
+}
