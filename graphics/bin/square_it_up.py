2bde02c1eff28cfd2f9c9bfa1d76c10e4be20823
Amir Baer <amir@curv.cc>
2021-03-24 23:03:53 +0200

    square it up

---
+++ graphics/bin/square_it_up.py
@@ -0,0 +1,59 @@
+#!/usr/local/bin/python3
+
+import random
+import os
+import sys
+
+
+import matplotlib.pyplot as plt
+from numpy import random
+from shapely.geometry import box
+from shapely.geometry import MultiPolygon
+
+# trick for making local package imports work
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), ".."))
+
+def plot_coords(coords):
+    pts = list(coords)
+    x,y = zip(*pts)
+    plt.plot(x,y)
+
+def plot_polys(polys):
+    for poly in polys:
+        if (not getattr(poly, "exterior", None)):
+            print("got line?")
+
+        plot_coords(poly.exterior.coords)
+
+        for hole in poly.interiors:
+            plot_coords(hole.coords)
+
+def babushka_squares(output, n):
+    boxes = []
+    for i in range(n):
+        boxes.append(box(i, i, n - i, n - i))
+    boxes = MultiPolygon(boxes)
+    plot_polys(boxes)
+    plt.axis("off")
+    plt.savefig(output)
+
+def square_mosaic(output, n):
+    data = random.random((n,n))
+    img = plt.imshow(data, interpolation='nearest')
+    img.set_cmap('hot')
+    plt.axis('off')
+    plt.savefig(output, bbox_inches='tight')
+
+
+def square_it_up():
+    if not len(sys.argv) == 3:
+        print("usage: %s <output> <n>" % sys.argv[0])
+        sys.exit(1)
+
+    output = sys.argv[1]
+    n = int(sys.argv[2])
+    square_mosaic(output, n)
+    #babushka_squares(output, n)
+
+if __name__ == "__main__":
+    square_it_up()
