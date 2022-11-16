2cea31d2e804d02c6c312aa52430d5dbb93db579
Amir Baer <baeramir@gmail.com>
2022-11-16 10:18:07 +0200

    Update compare_folders.sh

--- files/compare_folders.sh
+++ files/compare_folders.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [[ $# != 2 ]]; then
-	echo "usage: $0 <source dir> <dest dir>"
+if [[ $# < 2 ]]; then
+	echo "usage: $0 <source dir> <dest dir> [<extension=HEIC>]"
 	exit 1
 fi
 
 source_dir="$1"
 dest_dir="$2"
+extension="${3-HEIC}"
 
 work_dir="/tmp/cf-$RANDOM"
 mkdir "$work_dir"
@@ -14,10 +15,10 @@ echo "work dir: $work_dir"
 
 IFS=$'\n'
 
-s1="$work_dir/01_source_heic.txt"
-s2="$work_dir/02_source_sorted_sized_heic.txt"
+s1="$work_dir/01_source_$extension.txt"
+s2="$work_dir/02_source_sorted_sized_$extension.txt"
 d1=`echo $s1 | sed 's/source/dest/g'`
-d2=`echo $s1 | sed 's/source/dest/g'`
+d2=`echo $s2 | sed 's/source/dest/g'`
 
 process_dir () {
 	id="$1"
@@ -26,8 +27,8 @@ process_dir () {
 	category="$4"
 
 	# TODO: this only looks at HEIC files, we will need MOV videos and perhaps other file types as well
-	echo -n "reading $category dir... "
-	find "$id" -type f -iname '*.HEIC' -exec realpath {} ';' > "$of1"
+	echo -n "reading $category dir [$id]... "
+	find "$id" -type f -iname "*.$extension" -exec realpath {} ';' > "$of1"
 	echo "found `wc -l $of1 | awk '{print $1}'` files"
 
 	echo "checking file sizes..."
@@ -40,16 +41,16 @@ process_dir () {
 	done
 
 	echo
-	echo "sorting file list..."
+	echo -n "sorting file list... "
 	sort -o "$of2"{,}
+	echo "done"
 }
 
 process_dir "$source_dir" "$s1" "$s2" "source"
 process_dir "$dest_dir" "$d1" "$d2" "dest"
 
-
-
 # Now we need to figure out how to compare these two lists
-# Well let's run the script first and see the lists
 
-echo "done"
+echo
+echo "all done, output files:"
+find "$work_dir" -type f -exec realpath {} ';' | sort -n
