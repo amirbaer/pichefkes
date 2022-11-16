27187c546921bda585020bc85d5f853177324263
Amir Baer <baeramir@gmail.com>
2022-11-16 10:00:15 +0200

    Update compare_folders.sh

--- files/compare_folders.sh
+++ files/compare_folders.sh
@@ -19,35 +19,35 @@ s2="$work_dir/02_source_sorted_sized_heic.txt"
 d1=`echo $s1 | sed 's/source/dest/g'`
 d2=`echo $s1 | sed 's/source/dest/g'`
 
-read_dir () {
-	d="$1"
-	f1="$2"
-	f2="$3"
+process_dir () {
+	id="$1"
+	of1="$2"
+	of2="$3"
 	category="$4"
 
 	# TODO: this only looks at HEIC files, we will need MOV videos and perhaps other file types as well
 	echo -n "reading $category dir... "
-	find "$d" -type f -iname '*.HEIC' -exec realpath {} ';' > "$s1"
-	echo "found `wc -l $s1 | awk '{print $1}'` files"
+	find "$id" -type f -iname '*.HEIC' -exec realpath {} ';' > "$of1"
+	echo "found `wc -l $of1 | awk '{print $1}'` files"
 
 	echo "checking file sizes..."
-	for f in `cat $s1`; do
+	for f in `cat $of1`; do
 		b=$(basename "$f")
 		pd=$(basename $(dirname "$f"))
 		fs=$(du -k "$f" | awk '{print $1}')
-		echo "$pd|$b|$fs|$f" >> "$s2"
+		echo "$pd|$b|$fs|$f" >> "$of2"
 		echo -n "."
 	done
 
 	echo
 	echo "sorting file list..."
-	sort -o "$s2"{,}
+	sort -o "$of2"{,}
 }
 
+process_dir "$source_dir" "$s1" "$s2" "source"
+process_dir "$dest_dir" "$d1" "$d2" "dest"
 
-exit
 
-for i in `find "$dest_dir" -type f -iname '*.HEIC' -exec realpath {} ';'`; do du "$i"; done | sort -n > "$work_dir/01b_dest_sorted_heic.txt"
 
 # Now we need to figure out how to compare these two lists
 # Well let's run the script first and see the lists
