046969e046099c3b6c38870ed84708e6dc9ffb18
Amir Baer <baeramir@gmail.com>
2023-05-19 14:02:19 +0300

    WIP
    chatgpt workin on it

---
+++ graphics/python/chatgpt/gradient_from_palette.py
@@ -0,0 +1,57 @@
+#!/usr/local/bin/python3
+
+#!/usr/local/bin/python3
+
+import os
+import argparse
+from PIL import Image
+import colorsys
+import tqdm
+
+def hex_to_rgb(value):
+    # Convert hex color to RGB
+    value = value.lstrip('#')
+    length = len(value)
+    return tuple(int(value[i:i+length//3], 16) for i in range(0, length, length//3))
+
+def create_gradient_frames(output_folder, colors, num_frames, img_size):
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Iterate over each frame
+    for frame in tqdm.tqdm(range(num_frames), desc="generating frames"):
+        # Create a new image with the given size
+        img = Image.new('RGB', img_size)
+
+        # Use a color from the spectrum
+        hue = (frame / (num_frames - 1)) * (len(colors) - 1)  # Hue value, from 0 to len(colors) - 1
+        color = colors[int(hue)]  # Convert hue to RGB
+
+        # Fill the image with the color
+        for i in range(img_size[0]):
+            for j in range(img_size[1]):
+                img.putpixel((i, j), color)
+
+        # Save the image with a batch prefix and a 5-digit index
+        img.save(os.path.join(output_folder, f'01_{frame + 1:05d}.png'))
+
+def main():
+    # Initialize the parser
+    parser = argparse.ArgumentParser(description="Generate gradient frames from a palette")
+    parser.add_argument('--output_folder', type=str, default='data/gradient_from_palette', help="Folder to output frames")
+    parser.add_argument('--palette', type=str, nargs='+', default=['#FF0000', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF'], help="List of hex colors")
+    parser.add_argument('--num_frames', type=int, default=100, help="Number of frames to generate")
+    parser.add_argument('--img_size', type=int, nargs=2, default=[3840, 2160], help="Image size [width, height]")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Normalize the colors to 0-255
+    colors = [tuple(map(lambda x: x/255, hex_to_rgb(color))) for color in args.palette]
+
+    # Call the function to create the frames
+    create_gradient_frames(args.output_folder, colors, args.num_frames, tuple(args.img_size))
+
+if __name__ == "__main__":
+    main()
