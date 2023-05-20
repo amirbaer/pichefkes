ee0dd25e3b477a782e0974a3ff1f221b92d4e407
Amir Baer <baeramir@gmail.com>
2023-05-20 20:31:58 +0300

    Update single_gradient_from_palette.py

--- graphics/python/chatgpt/single_gradient_from_palette.py
+++ graphics/python/chatgpt/single_gradient_from_palette.py
@@ -3,7 +3,7 @@
 import os
 import random
 import argparse
-from PIL import Image, ImageDraw
+from PIL import Image
 
 def get_palette_from_images(folder):
     """Extract the color palette from the image filenames in a folder."""
@@ -40,12 +40,12 @@ def main(args):
     for i in range(args.num_images):
         chosen_colors = random.sample(palette, args.colors_per_gradient)
         image = generate_gradient(chosen_colors, args.width, args.height, args.direction)
-        image.save(f'gradient_{i}.png')
+        image.save(os.path.join(args.output_folder, f'gradient_{i}.png'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate gradient images.')
-    parser.add_argument('--palette', type=str, required=True,
-                        help='A comma-separated list of colors, or a folder of single-color images named with their color.')
+    parser.add_argument('palette', type=str, help='A comma-separated list of colors, or a folder of single-color images named with their color.')
+    parser.add_argument('output_folder', type=str)
     parser.add_argument('--num-images', type=int, default=10,
                         help='The number of gradient images to generate.')
     parser.add_argument('--colors-per-gradient', type=int, default=2,
@@ -53,9 +53,11 @@ if __name__ == '__main__':
     parser.add_argument('--direction', type=str, default='horizontal',
                         choices=['horizontal', 'vertical'], 
                         help='The direction of the gradient.')
-    parser.add_argument('--width', type=int, default=500,
+    parser.add_argument('--width', type=int, default=3840,
                         help='The width of the generated images.')
-    parser.add_argument('--height', type=int, default=500,
+    parser.add_argument('--height', type=int, default=2160,
                         help='The height of the generated images.')
     args = parser.parse_args()
+
+
     main(args)
