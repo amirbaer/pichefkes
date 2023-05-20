623141aee6338ca96518c019bacbcf378e6ae798
Amir Baer <baeramir@gmail.com>
2023-05-20 20:19:05 +0300

    Update single_create_blur_from_source_images.py

--- graphics/python/chatgpt/single_create_blur_from_source_images.py
+++ graphics/python/chatgpt/single_create_blur_from_source_images.py
@@ -5,7 +5,7 @@ import os
 import argparse
 import tqdm
 
-def create_blurred_images(input_dir, output_dir, output_dimensions):
+def create_blurred_images(input_dir, output_dir, output_dimensions, blur_strength):
     # Get all files in the input directory
     files = os.listdir(input_dir)
     # Filter out non-image files
@@ -15,7 +15,7 @@ def create_blurred_images(input_dir, output_dir, output_dimensions):
         # Load the source image
         source_image = cv2.imread(os.path.join(input_dir, image_file))
         # Blur the image
-        image_blurred = cv2.GaussianBlur(source_image, (99, 99), 30)
+        image_blurred = cv2.GaussianBlur(source_image, (99, 99), blur_strength)
         # Resize it to the output dimensions
         image_resized = cv2.resize(image_blurred, output_dimensions)  # cv2 uses width x height
         # Write the image
@@ -27,10 +27,11 @@ if __name__ == "__main__":
     parser.add_argument('input_dir', type=str, help='Directory containing source images')
     parser.add_argument('output_dir', type=str, help='Directory to save the generated images')
     parser.add_argument('--output_dimensions', type=int, nargs=2, default=[3840, 2160], help='Output image dimensions')
+    parser.add_argument('-b', '--blur_strength', type=int, default=30, help='Strength of the blur')
     args = parser.parse_args()
 
     # Create the output directory if it doesn't exist
     os.makedirs(args.output_dir, exist_ok=True)
 
     # Generate blurred images
-    create_blurred_images(args.input_dir, args.output_dir, args.output_dimensions)
+    create_blurred_images(args.input_dir, args.output_dir, args.output_dimensions, args.blur_strength)
